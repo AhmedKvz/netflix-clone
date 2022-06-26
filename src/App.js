cd00e1a3b73ca535f4a940b0f591279d9a4e8a8f
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2022-06-27 01:42:12 +0200

    first dev commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from "react";
 import "./App.css";
 
 import HomeScreen from "./components/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = {
+    name: "ahmed",
+  };
   return (
     <div className="App">
-      <HomeScreen />
+      <BrowserRouter>
+        <Routes>
+          {!user ? (
+            <Route path="/login" element={<LoginScreen />}></Route>
+          ) : (
+            <Route path="/" element={<HomeScreen />}></Route>
+          )}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
